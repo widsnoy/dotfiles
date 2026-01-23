e9ae86a0c5663c1cc2377f2549283c79cb17ea6c
widsnoy <ricofx47@outlook.com>
2026-01-23 15:53:29 +0800

    feat(nvim): add ensure_installed ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,13 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "ts_ls",
+            "jsonls",
+            "clangd",
+        },
+    },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
